918f37cac2e34d5e09f8e2e6392527a44141ca34
Aidan Krishnan <El1mination519@gmail.com>
2023-08-15 17:44:00 -0700

    Create Variables and Set Them on The ShooterAnimInstance Class

--- Source/Shooter/ShooterAnimInstance.cpp
+++ Source/Shooter/ShooterAnimInstance.cpp
@@ -2,4 +2,39 @@
 
 
 #include "ShooterAnimInstance.h"
+#include "ShooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if (!ShooterCharacter) // Makes sure ShooterCharacter isn't null
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+	}
+
+	if (ShooterCharacter)
+	{
+		// Set Speed
+		FVector Velocity = ShooterCharacter->GetVelocity();
+		Velocity.Z = 0;
+		Speed = Velocity.Size();
+
+		// Set IsInAir
+		bIsInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();
+
+		// Set Is Accelerating
+		if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f)
+		{
+			bIsAccelerating = true;
+		}
+		else
+		{
+			bIsAccelerating = false;
+		}
+	}
+}
+
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+}
--- Source/Shooter/ShooterAnimInstance.h
+++ Source/Shooter/ShooterAnimInstance.h
@@ -13,5 +13,24 @@ UCLASS()
 class SHOOTER_API UShooterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+	virtual void NativeInitializeAnimation() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	class AShooterCharacter* ShooterCharacter;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
 };
