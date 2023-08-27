7832f783168f33bcde77013c2df60c83d3065f9d
Aidan Krishnan <El1mination519@gmail.com>
2023-08-27 09:23:34 -0700

    Added Item Class

---
+++ Source/Shooter/Item.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item.h"
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
